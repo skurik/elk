888676b6147432cb35eaa16d6f6275e94c95046c
standa.kurik <standa.kurik@gmail.com>
2017-03-16 10:02:48 +0100

    Add simple script for moving iQube IIS logs from a temp location to a directory where filebeat eats it

---
+++ share/scripts/copylogs.sh
@@ -0,0 +1,210 @@
+#!/bin/bash
+mv /storage/logs/temp/iqube/u_ex161201.log /storage/logs/iis/iqube/
+sleep 360
+mv /storage/logs/temp/iqube/u_ex161202.log /storage/logs/iis/iqube/
+sleep 360
+mv /storage/logs/temp/iqube/u_ex161203.log /storage/logs/iis/iqube/
+sleep 360
+mv /storage/logs/temp/iqube/u_ex161204.log /storage/logs/iis/iqube/
+sleep 360
+mv /storage/logs/temp/iqube/u_ex161205.log /storage/logs/iis/iqube/
+sleep 360
+mv /storage/logs/temp/iqube/u_ex161206.log /storage/logs/iis/iqube/
+sleep 360
+mv /storage/logs/temp/iqube/u_ex161207.log /storage/logs/iis/iqube/
+sleep 360
+mv /storage/logs/temp/iqube/u_ex161208.log /storage/logs/iis/iqube/
+sleep 360
+mv /storage/logs/temp/iqube/u_ex161209.log /storage/logs/iis/iqube/
+sleep 360
+mv /storage/logs/temp/iqube/u_ex161210.log /storage/logs/iis/iqube/
+sleep 360
+mv /storage/logs/temp/iqube/u_ex161211.log /storage/logs/iis/iqube/
+sleep 360
+mv /storage/logs/temp/iqube/u_ex161212.log /storage/logs/iis/iqube/
+sleep 360
+mv /storage/logs/temp/iqube/u_ex161213.log /storage/logs/iis/iqube/
+sleep 360
+mv /storage/logs/temp/iqube/u_ex161214.log /storage/logs/iis/iqube/
+sleep 360
+mv /storage/logs/temp/iqube/u_ex161215.log /storage/logs/iis/iqube/
+sleep 360
+mv /storage/logs/temp/iqube/u_ex161216.log /storage/logs/iis/iqube/
+sleep 360
+mv /storage/logs/temp/iqube/u_ex161217.log /storage/logs/iis/iqube/
+sleep 360
+mv /storage/logs/temp/iqube/u_ex161218.log /storage/logs/iis/iqube/
+sleep 360
+mv /storage/logs/temp/iqube/u_ex161219.log /storage/logs/iis/iqube/
+sleep 360
+mv /storage/logs/temp/iqube/u_ex161220.log /storage/logs/iis/iqube/
+sleep 360
+mv /storage/logs/temp/iqube/u_ex161221.log /storage/logs/iis/iqube/
+sleep 360
+mv /storage/logs/temp/iqube/u_ex161222.log /storage/logs/iis/iqube/
+sleep 360
+mv /storage/logs/temp/iqube/u_ex161223.log /storage/logs/iis/iqube/
+sleep 360
+mv /storage/logs/temp/iqube/u_ex161224.log /storage/logs/iis/iqube/
+sleep 360
+mv /storage/logs/temp/iqube/u_ex161225.log /storage/logs/iis/iqube/
+sleep 360
+mv /storage/logs/temp/iqube/u_ex161226.log /storage/logs/iis/iqube/
+sleep 360
+mv /storage/logs/temp/iqube/u_ex161227.log /storage/logs/iis/iqube/
+sleep 360
+mv /storage/logs/temp/iqube/u_ex161228.log /storage/logs/iis/iqube/
+sleep 360
+mv /storage/logs/temp/iqube/u_ex161229.log /storage/logs/iis/iqube/
+sleep 360
+mv /storage/logs/temp/iqube/u_ex161230.log /storage/logs/iis/iqube/
+sleep 360
+mv /storage/logs/temp/iqube/u_ex161231.log /storage/logs/iis/iqube/
+sleep 360
+mv /storage/logs/temp/iqube/u_ex170101.log /storage/logs/iis/iqube/
+sleep 360
+mv /storage/logs/temp/iqube/u_ex170102.log /storage/logs/iis/iqube/
+sleep 360
+mv /storage/logs/temp/iqube/u_ex170103.log /storage/logs/iis/iqube/
+sleep 360
+mv /storage/logs/temp/iqube/u_ex170104.log /storage/logs/iis/iqube/
+sleep 360
+mv /storage/logs/temp/iqube/u_ex170105.log /storage/logs/iis/iqube/
+sleep 360
+mv /storage/logs/temp/iqube/u_ex170106.log /storage/logs/iis/iqube/
+sleep 360
+mv /storage/logs/temp/iqube/u_ex170107.log /storage/logs/iis/iqube/
+sleep 360
+mv /storage/logs/temp/iqube/u_ex170108.log /storage/logs/iis/iqube/
+sleep 360
+mv /storage/logs/temp/iqube/u_ex170109.log /storage/logs/iis/iqube/
+sleep 360
+mv /storage/logs/temp/iqube/u_ex170110.log /storage/logs/iis/iqube/
+sleep 360
+mv /storage/logs/temp/iqube/u_ex170111.log /storage/logs/iis/iqube/
+sleep 360
+mv /storage/logs/temp/iqube/u_ex170112.log /storage/logs/iis/iqube/
+sleep 360
+mv /storage/logs/temp/iqube/u_ex170113.log /storage/logs/iis/iqube/
+sleep 360
+mv /storage/logs/temp/iqube/u_ex170114.log /storage/logs/iis/iqube/
+sleep 360
+mv /storage/logs/temp/iqube/u_ex170115.log /storage/logs/iis/iqube/
+sleep 360
+mv /storage/logs/temp/iqube/u_ex170116.log /storage/logs/iis/iqube/
+sleep 360
+mv /storage/logs/temp/iqube/u_ex170117.log /storage/logs/iis/iqube/
+sleep 360
+mv /storage/logs/temp/iqube/u_ex170118.log /storage/logs/iis/iqube/
+sleep 360
+mv /storage/logs/temp/iqube/u_ex170119.log /storage/logs/iis/iqube/
+sleep 360
+mv /storage/logs/temp/iqube/u_ex170120.log /storage/logs/iis/iqube/
+sleep 360
+mv /storage/logs/temp/iqube/u_ex170121.log /storage/logs/iis/iqube/
+sleep 360
+mv /storage/logs/temp/iqube/u_ex170122.log /storage/logs/iis/iqube/
+sleep 360
+mv /storage/logs/temp/iqube/u_ex170123.log /storage/logs/iis/iqube/
+sleep 360
+mv /storage/logs/temp/iqube/u_ex170124.log /storage/logs/iis/iqube/
+sleep 360
+mv /storage/logs/temp/iqube/u_ex170125.log /storage/logs/iis/iqube/
+sleep 360
+mv /storage/logs/temp/iqube/u_ex170126.log /storage/logs/iis/iqube/
+sleep 360
+mv /storage/logs/temp/iqube/u_ex170127.log /storage/logs/iis/iqube/
+sleep 360
+mv /storage/logs/temp/iqube/u_ex170128.log /storage/logs/iis/iqube/
+sleep 360
+mv /storage/logs/temp/iqube/u_ex170129.log /storage/logs/iis/iqube/
+sleep 360
+mv /storage/logs/temp/iqube/u_ex170130.log /storage/logs/iis/iqube/
+sleep 360
+mv /storage/logs/temp/iqube/u_ex170131.log /storage/logs/iis/iqube/
+sleep 360
+mv /storage/logs/temp/iqube/u_ex170201.log /storage/logs/iis/iqube/
+sleep 360
+mv /storage/logs/temp/iqube/u_ex170202.log /storage/logs/iis/iqube/
+sleep 360
+mv /storage/logs/temp/iqube/u_ex170203.log /storage/logs/iis/iqube/
+sleep 360
+mv /storage/logs/temp/iqube/u_ex170204.log /storage/logs/iis/iqube/
+sleep 360
+mv /storage/logs/temp/iqube/u_ex170205.log /storage/logs/iis/iqube/
+sleep 360
+mv /storage/logs/temp/iqube/u_ex170206.log /storage/logs/iis/iqube/
+sleep 360
+mv /storage/logs/temp/iqube/u_ex170207.log /storage/logs/iis/iqube/
+sleep 360
+mv /storage/logs/temp/iqube/u_ex170208.log /storage/logs/iis/iqube/
+sleep 360
+mv /storage/logs/temp/iqube/u_ex170209.log /storage/logs/iis/iqube/
+sleep 360
+mv /storage/logs/temp/iqube/u_ex170210.log /storage/logs/iis/iqube/
+sleep 360
+mv /storage/logs/temp/iqube/u_ex170211.log /storage/logs/iis/iqube/
+sleep 360
+mv /storage/logs/temp/iqube/u_ex170212.log /storage/logs/iis/iqube/
+sleep 360
+mv /storage/logs/temp/iqube/u_ex170213.log /storage/logs/iis/iqube/
+sleep 360
+mv /storage/logs/temp/iqube/u_ex170214.log /storage/logs/iis/iqube/
+sleep 360
+mv /storage/logs/temp/iqube/u_ex170215.log /storage/logs/iis/iqube/
+sleep 360
+mv /storage/logs/temp/iqube/u_ex170216.log /storage/logs/iis/iqube/
+sleep 360
+mv /storage/logs/temp/iqube/u_ex170217.log /storage/logs/iis/iqube/
+sleep 360
+mv /storage/logs/temp/iqube/u_ex170218.log /storage/logs/iis/iqube/
+sleep 360
+mv /storage/logs/temp/iqube/u_ex170219.log /storage/logs/iis/iqube/
+sleep 360
+mv /storage/logs/temp/iqube/u_ex170220.log /storage/logs/iis/iqube/
+sleep 360
+mv /storage/logs/temp/iqube/u_ex170221.log /storage/logs/iis/iqube/
+sleep 360
+mv /storage/logs/temp/iqube/u_ex170222.log /storage/logs/iis/iqube/
+sleep 360
+mv /storage/logs/temp/iqube/u_ex170223.log /storage/logs/iis/iqube/
+sleep 360
+mv /storage/logs/temp/iqube/u_ex170224.log /storage/logs/iis/iqube/
+sleep 360
+mv /storage/logs/temp/iqube/u_ex170225.log /storage/logs/iis/iqube/
+sleep 360
+mv /storage/logs/temp/iqube/u_ex170226.log /storage/logs/iis/iqube/
+sleep 360
+mv /storage/logs/temp/iqube/u_ex170227.log /storage/logs/iis/iqube/
+sleep 360
+mv /storage/logs/temp/iqube/u_ex170228.log /storage/logs/iis/iqube/
+sleep 360
+mv /storage/logs/temp/iqube/u_ex170301.log /storage/logs/iis/iqube/
+sleep 360
+mv /storage/logs/temp/iqube/u_ex170302.log /storage/logs/iis/iqube/
+sleep 360
+mv /storage/logs/temp/iqube/u_ex170303.log /storage/logs/iis/iqube/
+sleep 360
+mv /storage/logs/temp/iqube/u_ex170304.log /storage/logs/iis/iqube/
+sleep 360
+mv /storage/logs/temp/iqube/u_ex170305.log /storage/logs/iis/iqube/
+sleep 360
+mv /storage/logs/temp/iqube/u_ex170306.log /storage/logs/iis/iqube/
+sleep 360
+mv /storage/logs/temp/iqube/u_ex170307.log /storage/logs/iis/iqube/
+sleep 360
+mv /storage/logs/temp/iqube/u_ex170308.log /storage/logs/iis/iqube/
+sleep 360
+mv /storage/logs/temp/iqube/u_ex170309.log /storage/logs/iis/iqube/
+sleep 360
+mv /storage/logs/temp/iqube/u_ex170310.log /storage/logs/iis/iqube/
+sleep 360
+mv /storage/logs/temp/iqube/u_ex170311.log /storage/logs/iis/iqube/
+sleep 360
+mv /storage/logs/temp/iqube/u_ex170312.log /storage/logs/iis/iqube/
+sleep 360
+mv /storage/logs/temp/iqube/u_ex170313.log /storage/logs/iis/iqube/
+sleep 360
+mv /storage/logs/temp/iqube/u_ex170314.log /storage/logs/iis/iqube/
+sleep 360
+
